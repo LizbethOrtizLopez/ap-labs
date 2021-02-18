f0d6cea59865ecc260e47098ca1792d7a22105b8
Lizbeth Ortiz Lopez <a00227346@itesm.mx>
2021-02-17 18:11:54 -0600

    Solve c-basics lab
    Signed-off-by: Lizbeth Ortiz Lopez <a00227346@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,19 @@
+//Lizbeth Ortiz Lopez
+
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
-/* print Fahrenheit-Celsius table */
-
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+    if (argc==2){ 
+        printf("Fahrenheit: %3s, Celcius: %6.1f\n", argv[1], (5.0/9.0)*(atoi(argv[1])-32));
+    }
+    else if (argc==4){ 
+        for (int i=atoi(argv[1]);i<=atoi(argv[2]);i=i+atoi(argv[3]))
+        printf("Fahrenheit: %3d, Celcius: %6.1f\n", i, (5.0/9.0)*(i-32));
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-
+    }else{ 
+        printf("Error al ingresar los datos \n");
+    }
     return 0;
 }
