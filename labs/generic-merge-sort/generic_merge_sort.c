2e572e9cf4761ff27a43a3c32978479e35cfd875
LizbethOrtizLopez <A00227346@itesm.mx>
2021-03-20 23:21:10 +0000

    merge ready without compare
    Signed-off-by: LizbethOrtizLopez <A00227346@itesm.mx>

--- labs/generic-merge-sort/generic_merge_sort.c
+++ labs/generic-merge-sort/generic_merge_sort.c
@@ -4,7 +4,11 @@
 
 
 #define len_strings 100
-void merge(char *file, char *results);
+void read(char *file, char *results);
+int numcmp(char *s1, char *s2);
+void mergeSort(void* datos, int ini, int fin, int (*comparacion)(void *, void*));
+void merge(void* datos[], int l, int m, int r, int (*comparacion)(void *, void*));
+
 
 int main(int argc, char **argv)
 {
@@ -16,17 +20,17 @@ int main(int argc, char **argv)
     printf("argv en 1 %s \n",argv[1]);
     if (strcmp(argv[1],"-n")==0){
         printf("entre \n");
-        merge(argv[2],argv[4]); 
+        read(argv[2],argv[4]); 
     }
     else{
-        merge(argv[1],"reporte");
+        read(argv[1],"reporte");
     }
         
     //}
     return 0;
 }
 
-void merge(char *file, char *results){
+void read(char *file, char *results){
 
     FILE * archivo; 
     char * line = NULL;
@@ -50,6 +54,7 @@ void merge(char *file, char *results){
 
     int i =0;
 
+
     while((read = getline(&line, &len, archivo)) != -1){
         i++;
 
@@ -67,7 +72,85 @@ void merge(char *file, char *results){
         
     }
 
+    mergeSort(arr_int,0,i-1,)
+
     free(arr_int);
     free(arr_string);
 
 }
+
+int numcmp(char *s1, char *s2) {
+    int v1, v2;
+    v1 = atoi(s1);
+    v2 = atoi(s2);
+    if(v1 < v2) 
+        return -1;
+    if(v1 > v2)
+        return 1;
+    return 0;
+}
+
+void mergeSort(void* datos, int ini, int fin, int (*comparacion)(void *, void*))
+{
+    if(ini<fin)
+    {
+        int mid=fin+(ini+1)/2;
+        mergeSort(datos,ini,mid,(*comparacion));
+        mergeSort(datos,mid+1,fin,(*comparacion));
+        merge(datos,ini,mid,fin,(*comparacion));
+    }     
+}
+
+void merge(void* datos[], int l, int m, int r, int (*comparacion)(void *, void*))
+{     
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    void* L[n1];
+    void* R[n2];
+
+    for (i = 0; i < n1; i++){
+         L[i] = datos[l + i];
+    }
+    for (j = 0; j < n2; j++){
+        R[j] = datos[m + 1+ j];
+    }
+    
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if ((*comparacion)(L[i],R[j])<=0)
+        {
+            datos[k] = L[i];
+            i++;
+        }
+        else
+        {
+            datos[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        datos[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        datos[k] = R[j];
+        j++;
+        k++;
+    }    
+}
+/*void imprimeArreglo(void* arr)
+{
+    for (E valor:arr)
+    {
+        System.out.print(valor+" ");
+    }
+    System.out.println("");
+}*/
