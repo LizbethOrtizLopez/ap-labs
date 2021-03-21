923c39950564407f8889a55f10c0254e7a8f5783
LizbethOrtizLopez <A00227346@itesm.mx>
2021-03-21 06:43:07 +0000

    seguimiento en merge
    Signed-off-by: LizbethOrtizLopez <A00227346@itesm.mx>

--- labs/generic-merge-sort/generic_merge_sort.c
+++ labs/generic-merge-sort/generic_merge_sort.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 
 #define len_strings 100
 void read(char *file, char *results);
 int numcmp(char *s1, char *s2);
-void mergeSort(void* datos, int ini, int fin, int (*comparacion)(void *, void*));
+void mergeSort(void* datos[], int ini, int fin, int (*comparacion)(void *, void*));
 void merge(void* datos[], int l, int m, int r, int (*comparacion)(void *, void*));
 
 
@@ -28,6 +29,94 @@ int main(int argc, char **argv)
         
     //}
     return 0;
+}
+
+int numcmp(char *s1, char *s2) {
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if(v1 < v2) 
+        return -1;
+    if(v1 > v2)
+        return 1;
+    return 0;
+}
+
+void mergeSort(void* datos[], int ini, int fin, int (*comparacion)(void *, void*))
+{
+    int numeric = 1;
+    if(ini<fin)
+    {
+        int mid=(fin+ini)/2;
+        mergeSort(datos,ini,mid,(*comparacion));
+        mergeSort(datos,mid+1,fin,(*comparacion));
+        merge(datos,ini,mid,fin,comparacion);
+    }   
+    else{
+        return;
+    } 
+}
+
+void merge(void* datos[], int l, int m, int r, int (*comparacion)(void *, void*))
+{  
+    printf("hiiiii \n");   
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    void* L[n1];
+    void* R[n2];
+
+    printf("esto es la r %d",r);
+
+    printf("estos son los datos: ");
+
+    for (int i=0;i<r;i++){
+        printf("%d ",datos[i]);
+    }
+
+    printf("\n aqui sigo \n");
+
+    for (i = 0; i < n1; i++){
+         L[i] = datos[l + i];
+    }
+    for (j = 0; j < n2; j++){
+        R[j] = datos[m + 1+ j];
+    }
+
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    printf("aqui sigo 2 \n");
+    while (i < n1 && j < n2)
+    {
+        printf("oa \n");
+        if ((*comparacion)(L[i],R[j])<0)
+        {
+            datos[k] = L[i];
+            i++;
+        }
+        else
+        {
+            datos[k] = R[j];
+            j++;
+        }
+        k++;
+       
+    }
+    while (i < n1)
+    {
+        datos[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        datos[k] = R[j];
+        j++;
+        k++;
+    }    
 }
 
 void read(char *file, char *results){
@@ -53,7 +142,14 @@ void read(char *file, char *results){
     arr_string = malloc(size_string * sizeof(char*));
 
     int i =0;
+    int numeric = 0;
 
+    if (strcmp(file,"numbers.txt")==0){
+        numeric = 1;
+    }
+    else{
+        numeric = 0;
+    }
 
     while((read = getline(&line, &len, archivo)) != -1){
         i++;
@@ -71,81 +167,26 @@ void read(char *file, char *results){
         }
         
     }
+    //mergeSort(arr_int,0,i-1,(int (*)(void *, void *))numeric?numcmp:strcmp);
 
-    mergeSort(arr_int,0,i-1,)
+    printf("hi \n");
+
+    printf(" esta es la i %d \n", i);
+
+
+    /*printf("estos son los datos 1: ");
+    for (int i=0;i<sizeof(arr_int);i++){
+        printf("%d ",arr_int[i]);
+    }*/
+
+    numeric ? mergeSort(arr_int,0,i, (int (*)(void*, void*))(numcmp)) : mergeSort(arr_int,0,i, (int (*)(void*, void*))(strcmp));
+
 
     free(arr_int);
     free(arr_string);
 
 }
 
-int numcmp(char *s1, char *s2) {
-    int v1, v2;
-    v1 = atoi(s1);
-    v2 = atoi(s2);
-    if(v1 < v2) 
-        return -1;
-    if(v1 > v2)
-        return 1;
-    return 0;
-}
-
-void mergeSort(void* datos, int ini, int fin, int (*comparacion)(void *, void*))
-{
-    if(ini<fin)
-    {
-        int mid=fin+(ini+1)/2;
-        mergeSort(datos,ini,mid,(*comparacion));
-        mergeSort(datos,mid+1,fin,(*comparacion));
-        merge(datos,ini,mid,fin,(*comparacion));
-    }     
-}
-
-void merge(void* datos[], int l, int m, int r, int (*comparacion)(void *, void*))
-{     
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    void* L[n1];
-    void* R[n2];
-
-    for (i = 0; i < n1; i++){
-         L[i] = datos[l + i];
-    }
-    for (j = 0; j < n2; j++){
-        R[j] = datos[m + 1+ j];
-    }
-    
-    i = 0;
-    j = 0;
-    k = l;
-    while (i < n1 && j < n2)
-    {
-        if ((*comparacion)(L[i],R[j])<=0)
-        {
-            datos[k] = L[i];
-            i++;
-        }
-        else
-        {
-            datos[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1)
-    {
-        datos[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
-    {
-        datos[k] = R[j];
-        j++;
-        k++;
-    }    
-}
 /*void imprimeArreglo(void* arr)
 {
     for (E valor:arr)
