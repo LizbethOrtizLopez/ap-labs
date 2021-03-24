c13de33a032710012f8c9100fb85d5662cb6b0ee
LizbethOrtizLopez <A00227346@itesm.mx>
2021-03-24 00:37:15 +0000

    strlib solve
    Signed-off-by: LizbethOrtizLopez <A00227346@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,38 @@
-#include <stdio.h>
+//Lizbeth Ortiz Lopez
+//A00227346
 
-int main() {
+#include <stdio.h>
+#include <string.h>
+
+int mystrlen(char *);
+char *mystradd(char *, char *);
+int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    if (argc==4 && strcmp(argv[1],"-find")==0){
+         int buscar = mystrfind(argv[2], argv[3]);
+        if (buscar > -1) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], buscar);
+        } else {
+            printf("The input was not found \n");
+        }
+        return 0;  
+    }
+    else if (argc==4 && strcmp(argv[1],"-add")==0){
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *added_word = mystradd(argv[2],argv[3]);
+        printf("New String          : %s\n", added_word);
+        printf("New length          : %d\n", mystrlen(added_word));
+        return 0;
+    }
+    else if (argc==3 && strcmp(argv[1],"-add")==0){
+        printf("Error. There is no string to add. Check the parameters. \n");
+    }
+    else if (argc==3 && strcmp(argv[1],"-find")==0){
+        printf("Error. There is no string to find. Check the parameters. \n");
+    }
+    else {
+        printf("Error. The input or the parameters where wrong \n");
+    }
     return 0;
 }
