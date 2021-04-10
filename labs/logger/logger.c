ad41ac091f69f1e6b9a1d8f146d20232bb500843
LizbethOrtizLopez <A00227346@itesm.mx>
2021-04-10 22:54:49 +0000

    Solve logger
    Signed-off-by: LizbethOrtizLopez <A00227346@itesm.mx>

---
+++ labs/logger/logger.c
@@ -0,0 +1,39 @@
+#include<stdio.h> 
+#include<stdarg.h>		
+#include<signal.h>
+
+#define backgroud 48 //black
+
+void setColor(int fg, int bg){
+    printf("%c[0;%d;%dm",0x1B, fg, bg);
+}
+
+void infof(const char *format, ...){
+    setColor(35,backgroud); //information in pink
+    va_list args;
+    va_start(args,format);
+    vprintf(format,args);
+    va_end(args);
+}
+void warnf(const char *format, ...){
+    setColor(33,backgroud); //warning in yellow
+    va_list args;
+    va_start(args,format);
+    vprintf(format,args);
+    va_end(args);
+}
+void errorf(const char *format, ...){
+    setColor(31,backgroud); //error in red
+    va_list args;
+    va_start(args,format);
+    vprintf(format,args);
+    va_end(args);
+}
+void panicf(const char *format, ...){ 
+    setColor(36,backgroud); //panic in cyan
+    va_list args;
+    va_start(args,format);
+    vprintf(format,args);
+    va_end(args);
+    //raise(SIGABRT);
+}
--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,21 @@
+#include <stdio.h>
+#include<stdlib.h>
 
+int infof(const char *format, ...);
+int warnf(const char *format, ...);
+int errorf(const char *format, ...);
+int panicf(const char *format, ...);
 
 int main() {
+    char *info = "Lizbeth Ortiz";
+    char *warning = "Warning, there is a pandemic";
+    char *error = "This code is wrong";
+    char *panic = "Delete this or it will explode";
+
+    infof("Information: %s\n",info);
+    warnf("Warning: %s\n",warning);
+    errorf("Error: %s\n",error);
+    panicf("Panic: %s\n",panic);
+
     return 0;
 }
